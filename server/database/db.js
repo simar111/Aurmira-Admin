8c73bc46636e1b014dfa5dc9348c7fea10ce4b20
Simar111 <simarnarula1477@gmail.com>
2025-08-15 23:05:31 +0530

    done

--- server/database/db.js
+++ server/database/db.js
@@ -1,15 +1,13 @@
 const mongoose = require('mongoose');
-const db="Ecom";
+const db = 'NewEcom'; // Replace with your new database name
+
 const connectToDB = async () => {
-
   try {
-    const dbUri = process.env.MONGODB_URI; // This should be the cluster URI, without database name
-
+    const dbUri = process.env.MONGODB_URI; // Cluster URI without database name
     const connection = await mongoose.createConnection(`${dbUri}/${db}`, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-
     console.log(`Connected to MongoDB Database: ${db}`);
     return connection;
   } catch (error) {
