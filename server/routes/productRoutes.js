764e340c652a0c2c5f78e9bfa33a4115eea1d23f
Simar111 <simarnarula1477@gmail.com>
2025-08-15 23:13:12 +0530

    done

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -19,7 +19,7 @@ const upload = multer({
 });
 
 // Routes
-router.post('/', upload.array('images', 5), addProduct); // Add a new product
+router.post('/add', upload.array('images', 5), addProduct); // Add a new product
 router.get('/', getAllProducts); // Get all products
 router.get('/count', getProductCount); // Get total product count
 router.delete('/:id', deleteProduct); // Delete a product by ID
